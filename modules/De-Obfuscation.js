b4661c99febf74f9f5c3f2ba5ccfa47f5f6d5bb1
Anto Joseph <antojoseph007@gmail.com>
2016-11-03 11:23:25 +0000

    Update De-Obfuscation.js
    fixed a typo

--- modules/De-Obfuscation.js
+++ modules/De-Obfuscation.js
@@ -2,5 +2,5 @@ Dalvik.perform(function () {
              var TextToDeObfuscate= "2MI1KO";  // obfuscated data goes here 
              var ClassToHook = Dalvik.use("com.jni.anto.kalip.MainActivity");
              var Result = ClassToHook.unobfuscate(TextToDeObfuscate);
-             send("[*] Tex to original: " + getResult);
+             send("[*] Tex to original: " + Result);
          });
